010f18a03d9275f52a380eb2b5e352e2e643f982
roche <haoshichen@outlook.fr>
2018-07-08 01:25:36 +0200

    ajouter RecetteBundle

---
+++ src/RecetteBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace RecetteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('RecetteBundle:Default:index.html.twig');
+    }
+}
